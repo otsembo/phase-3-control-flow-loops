3473010a80b4f3ba49a9b775c2adfd4f46f460f1
Ian Okumu <okumu.otsembo@gmail.com>
2022-07-07 11:33:43 +0300

    Loop lab completed

--- looping.rb
+++ looping.rb
@@ -1,5 +1,11 @@
 def happy_new_year
   # your code here
+  counter = 10
+  until counter == 0
+    puts counter
+    counter -= 1
+  end
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +23,18 @@ end
 
 def fizzbuzz_printer
   # your code here
+  (1..100).each do |i|
+    puts fizzbuzz(i)
+  end
 end
 
 def reverse_string(str)
   # your code here
+  index = str.length - 1
+  out = ""
+  until index == -1
+    out = "#{out}#{str[index]}"
+    index -= 1
+  end
+  out
 end
